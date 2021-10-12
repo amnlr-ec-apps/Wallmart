587605e35f9fb55a0d196767883b76f596408127
Manohara77 <manohara.arm@gmail.com>
2021-10-12 07:08:32 +0530

    added java

---
+++ DBUtilities.java
@@ -0,0 +1,16 @@
+c1 = Computer()
+c1.age = 30
+c2 = Computer()
+
+#c1.name = "Manu"
+# c1.age = 12
+
+if c1.compare(c2):
+    print("They are same")
+else:
+    print("They are different")
+
+# c1.update()
+
+print(c1.name)
+print(c2.name)
---
+++ StringUtiles.java
@@ -0,0 +1,9 @@
+print("================end=============")
+#second program #
+class Computer:
+    pass
+    def config(self):
+        print("i5, 16gb, 1TB")
+
+com1 = Computer()
+com2 = Computer()
